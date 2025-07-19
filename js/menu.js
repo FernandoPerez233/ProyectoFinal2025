171245c6cbab645f8c1c98208f21c0672be3d4ce
FernandoPerez233 <al45722@un.edu.mx>
2025-07-18 19:04:54 -0600

    Agregué boton inicio

--- js/menu.js
+++ js/menu.js
@@ -38,3 +38,20 @@
                 });
             });
         });
+
+        const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+   
+         // Mostrar el botón cuando se desplaza hacia abajo
+         window.addEventListener('scroll', () => {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+               scrollToTopBtn.style.display = 'block';
+            } else {
+               scrollToTopBtn.style.display = 'none';
+            }
+         });
+   
+         // Volver al inicio al hacer clic en el botón
+         scrollToTopBtn.addEventListener('click', () => {
+               document.body.scrollTop = 0; // Para Safari
+               document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+         });
